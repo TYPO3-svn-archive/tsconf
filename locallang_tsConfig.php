1aab583faa8a9ceb035c81164dab9610f72592af
uherrmann <uherrmann@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-07-27 08:51:53 +0000

    correct typo
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tsconf/trunk@64902 735d13b6-9817-0410-8766-e36946ffe9aa

--- locallang_tsConfig.php
+++ locallang_tsConfig.php
@@ -1,7 +1,7 @@
 <?php
 $LOCAL_LANG = array(
   'default' => array(
-    'tceform.tt_content.header_layout.0'   => 'Layout',
+    'tceform.tt_content.header_layout'     => 'Layout',
     'tceform.tt_content.header_layout.0'   => 'Header 1',
     'tceform.tt_content.header_layout.2'   => 'Header 2',
     'tceform.tt_content.header_layout.3'   => 'Header 3',

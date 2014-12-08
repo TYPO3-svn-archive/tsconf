f54f0c0185b10a455c2e74708ef2968628c91784
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-08 22:49:46 +0000

    1.6.0 +++ #i0006
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tsconf/trunk@87211 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,11 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "tsconf".
- *
- * Auto generated 05-06-2014 00:05
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'TSconfig Pages and Users by extManager',
 	'description' => 'Configure the TSconfig for your pages and users with the mouse! tsconf offers the eight most commonly used porperties like page tree uids, activated extended view, activated clipboard, activated admin panel and cache management for editors among others.',
 	'category' => 'be',
 	'shy' => 0,
-	'version' => '1.5.0',
+	'version' => '1.6.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -34,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.5.0-6.2.99',
+			'typo3' => '6.0.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
--- ext_localconf.php
+++ ext_localconf.php
@@ -6,12 +6,12 @@ $_EXTCONF = unserialize( $_EXTCONF );
 
 if ( $_EXTCONF[ 'page_mod_web_layout_menu_function' ] )
 {
-  t3lib_extMgm::addPageTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/pageTSconfig_weblayout_menu_functions.txt">' );
+  \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/pageTSconfig_weblayout_menu_functions.txt">' );
 }
 
 if ( $_EXTCONF[ 'page_mod_web_list_enableButtons' ] )
 {
-  t3lib_extMgm::addPageTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/pageTSconfig_weblist_enablebuttons.txt">' );
+  \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/pageTSconfig_weblist_enablebuttons.txt">' );
 }
 
 switch ( $_EXTCONF[ 'tca_ttcontentimages' ] )
@@ -38,26 +38,26 @@ tt_content.image.20.addClassesCol.override.cObject.10.12.value = col-md-1 col-sm
 
 if ( $_EXTCONF[ 'user_options_filelist_enableButtons' ] )
 {
-  t3lib_extMgm::addUserTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/userTSconfig_filelist_enablebuttons.txt">' );
+  \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/userTSconfig_filelist_enablebuttons.txt">' );
 }
 
 if ( $_EXTCONF[ 'page_tceform_pages' ] )
 {
-  t3lib_extMgm::addPageTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/pageTSconfig_tceform_pages.txt">' );
+  \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/pageTSconfig_tceform_pages.txt">' );
 }
 
 if ( $_EXTCONF[ 'page_tceform_ttcontent' ] )
 {
-  t3lib_extMgm::addPageTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/pageTSconfig_tceform_ttcontent.txt">' );
+  \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/pageTSconfig_tceform_ttcontent.txt">' );
 }
 
 switch ( $_EXTCONF[ 'page_mod_shared_deflangauge' ] )
 {
   case('de'):
-    t3lib_extMgm::addUserTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/pageTSconfig_mod_shared_defaultlang_de.txt">' );
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/pageTSconfig_mod_shared_defaultlang_de.txt">' );
     break;
   case('en'):
-    t3lib_extMgm::addUserTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/pageTSconfig_mod_shared_defaultlang_en.txt">' );
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/pageTSconfig_mod_shared_defaultlang_en.txt">' );
     break;
   case(null):
   default:
@@ -67,21 +67,21 @@ switch ( $_EXTCONF[ 'page_mod_shared_deflangauge' ] )
 
 if ( $_EXTCONF[ 'user_adminpanel' ] )
 {
-  t3lib_extMgm::addUserTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/userTSconfig_adminPanel.txt">' );
+  \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/userTSconfig_adminPanel.txt">' );
 }
 
 if ( $_EXTCONF[ 'user_clearcachesystem' ] )
 {
-  t3lib_extMgm::addUserTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/userTSconfig_clearCacheSystem.txt">' );
+  \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/userTSconfig_clearCacheSystem.txt">' );
 }
 
 if ( $_EXTCONF[ 'user_options_cache' ] )
 {
-  t3lib_extMgm::addUserTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/userTSconfig_options_cache.txt">' );
+  \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/userTSconfig_options_cache.txt">' );
 }
 
 if ( $_EXTCONF[ 'user_options_pagetreeuid' ] )
 {
-  t3lib_extMgm::addUserTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/userTSconfig_options_pagetreeuid.txt">' );
+  \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig( '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tsconf/userTSconfig_options_pagetreeuid.txt">' );
 }
 ?>
